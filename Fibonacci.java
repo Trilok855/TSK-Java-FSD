b62a656619be2ce8b941685bddb012dc1ead5aa1
Trilok855 <83392426+Trilok855@users.noreply.github.com>
2021-04-29 23:17:22 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Fibonacci{
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter no of terms required:");
+		int num=scan.nextInt();
+		int n=0;
+		int n2=1;
+		int n3;
+		System.out.println(n);
+		System.out.println(n2);
+		for(int i=2;i<num;i++){
+			n3=n+n2;
+			System.out.println(n3);
+			n=n2;
+			n2=n3;
+		}
+
+	}
+}
