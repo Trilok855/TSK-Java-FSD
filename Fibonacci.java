794881c7c93758c70bd2a6f6371124f20b0bbe6a
Trilok855 <83392426+Trilok855@users.noreply.github.com>
2021-04-30 11:23:16 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Fibonacci{
+public class Fibonacci{
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter no of terms required:");
