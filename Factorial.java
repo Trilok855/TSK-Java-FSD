60277f6a34c06a115d56aa52af782bef243cad9f
Trilok855 <83392426+Trilok855@users.noreply.github.com>
2021-04-30 11:24:18 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Factorial{
+public class Factorial{
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter num:");
